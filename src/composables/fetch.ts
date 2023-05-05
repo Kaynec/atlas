bffa20b1a5227d40b6c079ae23a88c29611ff842
Kaynec <68915270+Kaynec@users.noreply.github.com>
2023-05-05 18:07:06 +0330

    fix: change

--- src/composables/fetch.ts
+++ src/composables/fetch.ts
@@ -1,6 +1,6 @@
 import { createFetch } from "@vueuse/core";
 
-const baseURL = "http://127.0.0.1:8000/";
+const baseURL = "http://217.218.179.15:8000/";
 export const useMyFetch = createFetch({
   baseUrl: baseURL,
 });
